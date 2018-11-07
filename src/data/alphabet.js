62712785bb5717fe77cc0b4af0fabd72fdf7f3f0
elalaouifaris <elalaouifaris@gmail.com>
2018-11-07 08:48:40 +0100

    Add data for 2 additional letters

--- src/data/alphabet.js
+++ src/data/alphabet.js
@@ -45,28 +45,75 @@ const alphabet = [
     },
   },
 
-{
-  index: 3,
-  isolated_form: "ت",
-  reading: "Ta'",
+  {
+    index: 3,
+    isolated_form: "ت",
+    reading: "Ta'",
+      letter_highlight: {
+        first_letter: {
+          text: "تبب",
+          start: 0,
+          end: 25
+        },
+        middle_letter: {
+          text: "بتب",
+          start: 25,
+          end: 40
+        },
+        last_letter: {
+          text: "ببت",
+          start: 40,
+          end: 90
+        },
+      },
+  },
+
+  {
+    index: 4,
+    isolated_form: "ث",
+    reading: "Tha'",
     letter_highlight: {
       first_letter: {
-        text: "تبب",
+        text: "ثبب",
         start: 0,
         end: 25
       },
       middle_letter: {
-        text: "بتب",
+        text: "بثب",
         start: 25,
         end: 40
       },
       last_letter: {
-        text: "ببت",
+        text: "ببث",
         start: 40,
         end: 90
       },
     },
-},
+  },
+
+  {
+    index: 5,
+    isolated_form: "ج",
+    reading: "Jiim",
+    letter_highlight: {
+      first_letter: {
+        text: "جبب",
+        start: 0,
+        end: 35
+      },
+      middle_letter: {
+        text: "بجب",
+        start: 20,
+        end: 50
+      },
+      last_letter: {
+        text: "ببج",
+        start: 45,
+        end: 90
+      },
+    },
+  },
+
 ];
 
 export default alphabet;

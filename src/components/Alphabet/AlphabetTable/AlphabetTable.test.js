98c6e69cc5bb1c299151ea45e8795e2a3990c274
elalaouifaris <elalaouifaris@gmail.com>
2018-11-11 13:48:02 +0100

    Add tests for AlphabetTable component

---
+++ src/components/Alphabet/AlphabetTable/AlphabetTable.test.js
@@ -0,0 +1,53 @@
+import React from 'react';
+import { configure, shallow, mount } from 'enzyme';
+import Adapter from 'enzyme-adapter-react-16';
+
+import AlphabetTable from './AlphabetTable';
+import LetterThumbnail from '../LetterThumbnail/LetterThumbnail';
+
+configure({adapter: new Adapter()});
+
+describe('<AlphabetTable/>', () => {
+  const letters = [{
+    index: 1,
+    isolated_form: "أ",
+    reading: "Alif"
+  },
+  {
+    index: 2,
+    isolated_form: "ب",
+    reading: "Ba'"
+  }];
+
+  it('Component renders without errors', () => {
+    const wrapper = shallow(<AlphabetTable letters={ letters }/>);
+    wrapper
+  });
+
+  it('should render a LetterThumbnail for each element in given letters list', () => {
+    const wrapper = shallow(<AlphabetTable letters={ letters }/>);
+    expect(wrapper.find(LetterThumbnail)).toHaveLength(letters.length)
+  });
+
+  it('should render the letter isolated form as LetterThumbnail letter prop', () => {
+    const wrapper = shallow(<AlphabetTable letters={ letters }/>);
+    expect(wrapper.find(LetterThumbnail).first().props())
+      .toHaveProperty("letter", letters[0].isolated_form)
+  });
+
+  it('should render the letter reading form as LetterThumbnail reading prop', () => {
+    const wrapper = shallow(<AlphabetTable letters={ letters }/>);
+    expect(wrapper.find(LetterThumbnail).first().props())
+      .toHaveProperty("reading", letters[0].reading)
+  });
+
+  it('when LetterTHumbnail is clicked should call selectedLetter method with corresponding index', () => {
+    const clicked = jest.fn();
+    const wrapper = mount(<AlphabetTable
+      letters={ letters } selectLetter={ clicked }/>);
+    const letter = wrapper.find(LetterThumbnail).first();
+    letter.simulate('click');
+    expect(clicked).toBeCalledWith(letters[0].index);
+
+  });
+});
